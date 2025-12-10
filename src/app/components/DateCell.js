a1d41226f2d9602e7f149b0ab7d8d4d8c85fa215
RogueStar112 <hildtgunnt@gmail.com>
2025-12-10 21:32:10 +0000

    calendar styling

--- src/app/components/DateCell.js
+++ src/app/components/DateCell.js
@@ -1,7 +1,10 @@
-export default function DateCell({ date }) {
+export default function DateCell({ shortDate, fullDate, isToday }) {
     return (
-        <div className="p-2 border rounded-lg">
-            {date}
+        <div>
+            {isToday ? 
+            <button className="p-1 sm:p-2 border rounded-lg bg-black text-white w-full hover:bg-blue-600 hover:text-white hover:duration-500 hover:cursor-pointer">{shortDate}</button> 
+            : 
+            <button className="p-1 sm:p-2 border rounded-lg w-full hover:bg-blue-600 hover:text-white hover:duration-500 hover:cursor-pointer">{shortDate}</button>}
         </div>
     );
 }
