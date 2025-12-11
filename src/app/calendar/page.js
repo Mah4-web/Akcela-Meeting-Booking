6562add00e0a2eff8cabad3457e27e5a082b58c4
RogueStar112 <hildtgunnt@gmail.com>
2025-12-11 13:31:36 +0000

    Calendar branch update

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -9,14 +9,78 @@ export default function Calendar() {
 
     return (
         
-        <div className="flex flex-col items-center justify-center p-8">
-            <h1 className="text-center font-extrabold">Akcela Booking Calendar</h1>
-            
-            <p>Select a date to book your 2 hour slot</p>
+        <div className="flex flex-col sm:flex-row p-8">
 
-            <div className="min-h-screen w-full">
+            <div className="flex flex-col">
+                <h1 className="text-center font-extrabold">Akcela Booking Calendar</h1>
+                
+                <p className="text-center">Select a date to book your 2 hour slot</p>
 
-            <CalendarMonth month={month} current_date={today} />
+                <div className="min-h-screen w-full min-w-[320px]">
+
+                    <CalendarMonth month={month} current_date={today} />
+
+                </div>
+
+            </div>
+
+            <div className="grow bg-green-500 w-full min-h-screen grid grid-cols-7 grid-rows-7 gap-2 [&>*]:bg-green-400 [&>*]:flex [&>*]:items-center [&>*]:justify-center p-8">
+
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>  
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
 
             </div>
 
