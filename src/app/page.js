01641b2c80a416ca6750df0fb6a2ee7c351148f2
Mahpara <mah4.para@gmail.com>
2025-12-11 10:12:33 +0000

    chore: fixing error

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,18 @@
 export default function HomePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center flex-col p-8">
+      <h1 className="text-3xl font-bold mb-2">Welcome to Akcela Meeting Booking</h1>
+      <p className="mb-6">This is the main page of the Akcela Meeting Booking application.</p>
 
-    return (
-
-        <div className="flex min-h-screen items-center justify-center flex-col p-8">
-
-            <h1>Welcome to Akcela Meeting Booking</h1>
-            <p>This is the main page of the Akcela Meeting Booking application.</p>
-
-            <div className="flex gap-4 mt-4">
-
-                <div className="bg-green-500 p-4 text-white rounded-lg">
-                    <a href="/sign-in">Sign In</a>
-                </div>
-
-                <div className="bg-green-500 p-4 text-white rounded-lg">
-                    <a href="/sign-up">Sign Up</a>
-                </div>
-
-            </div>
-
-      <div className="flex">
-        <div>
+      <div className="flex gap-4">
+        <div className="bg-green-500 p-4 text-white rounded-lg hover:bg-green-600 transition">
           <a href="/sign-in">Sign In</a>
         </div>
-    )
+
+        <div className="bg-green-500 p-4 text-white rounded-lg hover:bg-green-600 transition">
+          <a href="/sign-up">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  );
 }
