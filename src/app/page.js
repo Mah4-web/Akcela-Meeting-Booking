3be42beb6b12243e810dfa11002623aec3ff8e81
Mahpara <mah4.para@gmail.com>
2025-12-11 09:51:45 +0000

    chore: testing clerk again

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,18 @@
-<html>
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Akcela Meeting Booking</h1>
+      <p>This is the main page of the Akcela Meeting Booking application.</p>
 
-    <head>
-        <title>Akcela Meeting Booking</title>
-    </head>
-
-    <body>
-        <h1>Welcome to Akcela Meeting Booking</h1>
-        <p>This is the main page of the Akcela Meeting Booking application.</p>
-
-        <div className="flex">
-
-            <div>
-                <a href="/sign-in">Sign In</a>
-            </div>
-
-            <div>
-                <a href="/sign-up">Sign Up</a>
-            </div>
-
+      <div className="flex">
+        <div>
+          <a href="/sign-in">Sign In</a>
         </div>
 
-    </body>
-</html>
+        <div>
+          <a href="/sign-up">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  );
+}
