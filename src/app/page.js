8d87d966d5c09af98514273014964ea911120416
Mahpara <mah4.para@gmail.com>
2025-12-11 11:31:33 +0000

    chore: styled homepage and linked sign in and sign up page

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,31 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center flex-col p-8">
-      <h1 className="text-3xl font-bold mb-2">Welcome to Akcela Meeting Booking</h1>
-      <p className="mb-6">This is the main page of the Akcela Meeting Booking application.</p>
+    <main className="min-h-screen flex flex-col items-center justify-center px-4 bg-(--color-gray-light)">
+      <h1 className="text-4xl md:text-5xl font-bold mb-4 text-black text-center">
+        Welcome to Akcela Meeting Booking
+      </h1>
+        <p className="mb-8 text-(--color-gray-dark) text-center max-w-xl">
+        Effortlessly schedule, manage, and track all your meetings in one place.
+        Join Akcela today to simplify your workflow, collaborate seamlessly, and stay on top of your schedule.
+      </p>
 
-      <div className="flex gap-4">
-        <div className="bg-green-500 p-4 text-white rounded-lg hover:bg-green-600 transition">
-          <a href="/sign-in">Sign In</a>
-        </div>
+      <div className="flex flex-col md:flex-row gap-6">
+        <Link
+          href="/sign-in"
+          className="bg-(--color-glass-bg) backdrop-blur-md border border-(--color-glass-border) shadow-lg rounded-2xl px-8 py-4 text-black font-semibold text-center transition-all duration-500 hover:shadow-2xl hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-black"
+        >
+          Sign In
+        </Link>
 
-        <div className="bg-green-500 p-4 text-white rounded-lg hover:bg-green-600 transition">
-          <a href="/sign-up">Sign Up</a>
-        </div>
+        <Link
+          href="/sign-up"
+          className="bg-(--color-glass-bg) backdrop-blur-md border border-(--color-glass-border) shadow-lg rounded-2xl px-8 py-4 text-black font-semibold text-center transition-all duration-500 hover:shadow-2xl hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-black"
+        >
+          Sign Up
+        </Link>
       </div>
-    </div>
+    </main>
   );
 }
